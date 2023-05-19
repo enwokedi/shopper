dea900bdce54ebf409ba23a850a1a14be58c146b
Emmanuel Nwokedi <emmanuel.nwokedi@gmail.com>
2023-05-19 09:32:53 +0100

    Navbar extended

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,33 +1,75 @@
 <header class="p-3 bg-dark text-white">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="{{ URL::to('rentals') }}" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                    <use xlink:href="#bootstrap" />
-                </svg>
-            </a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">NGM Administration</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Users
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Motorcycles
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Rentals
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/rentals">Payments Due</a></li>
+                            <li><a class="dropdown-item" href="#">Menu Item</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Menu Item</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <div class="d-flex" style="padding-left: 5px;">
+                    @auth
+                    {{auth()->user()->name}}
+                    <div class="text-end">
+                        <a href="{{ route('logout.perform') }}" class="btn btn-outline-danger me-2">Logout</a>
+                    </div>
+                    @endauth
 
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/rentals" class="nav-link px-2 text-secondary">Rentals</a></li>
-                <li><a href="#" class="nav-link px-2 text-white"></a></li>
-                <li><a href="#" class="nav-link px-2 text-white"></a></li>
-                <li><a href="#" class="nav-link px-2 text-white"></a></li>
-                <li><a href="#" class="nav-link px-2 text-white"></a></li>
-            </ul>
-
-            @auth
-            {{auth()->user()->name}}
-            <div class="text-end">
-                <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
+                    @guest
+                    <div class="text-end">
+                        <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
+                        <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
+                    </div>
+                    @endguest
+                </div>
             </div>
-            @endauth
-
-            @guest
-            <div class="text-end">
-                <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
-                <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
-            </div>
-            @endguest
         </div>
-    </div>
+    </nav>
 </header>
